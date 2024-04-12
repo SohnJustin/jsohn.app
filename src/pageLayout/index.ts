1f0212fd00178f8ad275853759d34bd33c3dc186
Justin Sohn <sohnjustin2@gmail.com>
2024-04-11 21:42:42 -0700

    added skills

--- src/pageLayout/index.ts
+++ src/pageLayout/index.ts
@@ -2,5 +2,6 @@ import Home from "./Home";
 import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
+import Skills from "./Skills";
 
-export { Home, About, Projects, Contact };
+export { Home, About, Projects, Contact, Skills };
